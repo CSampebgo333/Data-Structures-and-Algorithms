d2d79543aad376ec262bfa1246a64d92cf196ac3
CSampebgo333 <clemsosampebgo7@gmail.com>
2025-02-05 03:17:56 +0000

    areAlmostEquals(str1, str2)

--- Week_04/src/Main.java
+++ Week_04/src/Main.java
@@ -1,7 +1,22 @@
 public class Main{
     public static void main(String[] args){
+        /*
+
         int[] array1 = {1, 2};
         int[] array2 = {3, 4};
         System.out.println(Solution1.mergeArray(array1, array2));
+
+        String str1 = "kelb";
+        String str2 = "kelb";
+
+        String str1 = "attack";
+        String str2 = "defend";
+
+         */
+
+        String str1 = "bank";
+        String str2 = "kanb";
+        System.out.println(Solution2.areAlmostEqual(str1, str2));
+
     }
 }
--- Week_04/src/Solution2.java
+++ Week_04/src/Solution2.java
@@ -1,2 +1,20 @@
 public class Solution2 {
+    public static boolean areAlmostEqual(String str1, String str2){
+        StringBuilder orderedString = new StringBuilder();
+        for (int i = 0; i < str1.length(); i++){
+            for (int j = 0; j < str2.length(); j++){
+                if (str1.charAt(i) == str2.charAt(j)){
+                    orderedString.append(str1.charAt(i));
+                }
+                else {
+                    continue;
+                }
+            }
+        }
+
+        if (str1.equals(orderedString.toString())){
+            return true;
+        }
+        return false;
+    }
 }
