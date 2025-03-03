4a1d9bc784fcc3b85a9fc76b56396d5da03d73ba
CSampebgo333 <clemsosampebgo7@gmail.com>
2025-03-03 19:11:15 +0000

    Circularly Linked List (Continuous Play):

--- Assignment_2 - problem_2/src/FullyFunctionalPlaylist.java
+++ Assignment_2 - problem_2/src/FullyFunctionalPlaylist.java
@@ -3,5 +3,24 @@ public class FullyFunctionalPlaylist {
     private Song tail;
     private int size;
 
+    public FullyFunctionalPlaylist(){
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    public void addAtEnd(Song song){
+        if (size == 0){
+            this.head = song;
+            this.tail = head;
+            size += 1;
+        }
+        else {
+            this.tail.next = song;
+            song.prev = tail;
+            song.next = head;
+            size += 1;
+        }
+    }
 
 }
