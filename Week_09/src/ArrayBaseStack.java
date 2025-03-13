79766b430eeb693ee269c4b67fa0aa9efcf25695
CSampebgo333 <clemsosampebgo7@gmail.com>
2025-03-13 20:09:20 +0000

    Setup for Implementation of Stack and Queue using different approaches

---
+++ Week_09/src/ArrayBaseStack.java
@@ -0,0 +1,3 @@
+public class ArrayBaseStack {
+    private int value;
+}
--- Week_09/src/Main.java
+++ Week_09/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String [] args){
-        System.out.println("Hello World!");
+
+        System.out.println("============ Test Array Base Stack ============");
+
+        System.out.println("============ Test Singly LinkedList Base Stack ============");
+
+        System.out.println("============ Test Doubly LinkedList Base Stack ============");
+
+        System.out.println("============ Test Array Base Queue ============");
+
+        System.out.println("============ Test Singly LinkedList Base Queue ============");
+
+        System.out.println("============ Test Doubly LinkedList Base Queue ============");
     }
 }
