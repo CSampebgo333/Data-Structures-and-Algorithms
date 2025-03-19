b26b8f7704a7bd41e89ec26b5cca742e4202882d
CSampebgo333 <clemsosampebgo7@gmail.com>
2025-03-19 19:31:42 +0000

    Design an array-based simple stack and queue

--- Week_09/src/ArrayBaseQueue.java
+++ Week_09/src/ArrayBaseQueue.java
@@ -1,2 +1,47 @@
 public class ArrayBaseQueue {
+    private Integer[] queue;
+    private int front;
+    private int rear;
+    private final int capacity;
+    private int size;
+
+    public ArrayBaseQueue(int capacity){
+        queue = new Integer[capacity];
+        front = rear = -1;
+        this.capacity = capacity;
+        size = 0;
+    }
+
+    public boolean isEmpty(){
+        return front == -1;
+    }
+
+    public Integer front() throws Exception {
+        if (isEmpty()){
+            throw new Exception("No Item Found!");
+        }
+        else {
+            return queue[front];
+        }
+    }
+
+    public void dequeue() throws Exception{
+        if (isEmpty()){
+            throw new Exception("Queue underflow!");
+        }
+        else {
+            queue[front++] = null;
+            size --;
+        }
+    }
+
+    public void enqueue(Integer item) throws Exception {
+        if (size == capacity){
+            throw new Exception("Queue Overflow!");
+        }
+        else {
+            queue[++rear] = item;
+            size ++;
+        }
+    }
 }
--- Week_09/src/ArrayBaseStack.java
+++ Week_09/src/ArrayBaseStack.java
@@ -1,3 +1,47 @@
 public class ArrayBaseStack {
-    private int value;
+    private Integer[] stack;
+    private int size;
+    private int top;
+    private final int capacity;
+
+    public ArrayBaseStack(int capacity){
+        stack = new Integer[capacity];
+        top = -1;
+        this.capacity = capacity;
+        size = 0;
+    }
+
+    public boolean isEmpty(){
+        return top == -1;
+    }
+
+    public void pop() throws Exception{
+        if (isEmpty()){
+            throw new Exception("Stack underflow!");
+        }
+        else {
+            stack[top] = null;
+            top -= 1;
+            size -= 1;
+        }
+    }
+
+    public Integer top() throws Exception{
+        if (isEmpty()){
+            throw new Exception("No Item Found!");
+        }
+        else {
+            return stack[top];
+        }
+    }
+
+    public void push(Integer item) throws Exception {
+        if (size == capacity){
+            throw new Exception("Stack Overflow!");
+        }
+        else {
+            stack[++top] = item;
+            size += 1;
+        }
+    }
 }
